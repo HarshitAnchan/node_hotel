c88d94905af20f7d3057ed523d9f9f9eefbad9e4
harshitanchan <harshitanchan647@gmail.com>
2024-06-19 21:19:51 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -17,65 +17,6 @@ app.get('/', function (req, res) {
 })
 
 
-// post route to add a person
-// app.post('/person', async (req, res) => { // Corrected endpoint path
-//   try {
-//     const data = req.body; // Assuming the req body contains the person data
-
-//     // Create a new document using the mongoose model
-//     const newPerson = new Person(data);
-
-//     // Save the new person
-//     const response = await newPerson.save();
-//     console.log('Data saved');
-//     res.status(201).json(response);
-//   } catch (err) {
-//     console.log('Error saving data:', err);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
-
-
-
-// Get method to get the person
-// app.get('/person' , async (req, res) =>{
-
-//   try{
-//     const data = await Person.find();
-//     console.log('Data fetched');
-//     res.status(200).json(data);
-
-//   }catch(err){
-//     console.log(err);
-//     res.status(500).json({ error: 'Internal server error' });
-
-//   }
-// })
-
-
-
-
-// app.get('/person/:workType', async (req, res)=>{
-
-//  try{
-//   const workType =req.params.workType;
-//   if(workType == 'chef' || workType == 'manager' || workType =='waiter'){
-    
-//     const response = await Person.find({work: workType})
-//     console.log('response detched');
-//     res.status(200).json(response)
-//   }else{
-//     res.status(404).json({error: 'invalid worktype'})
-//   }
-
-//  }catch(err){
-//   console.log(err);
-//   res.status(500).json({ error: 'Internal server error' });
-
-
-//  }
-// })
-
 
 //Import the router files
 const personRoutes = require('./routes/personRoutes');
